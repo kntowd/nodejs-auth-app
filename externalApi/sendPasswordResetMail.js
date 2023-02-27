94f63554e1fe340043471e799dbc201fcfd3f50e
kenta owada <53945259+kntowd@users.noreply.github.com>
2023-02-27 14:22:40 +0900

    パスワードリセット機能を追加

---
+++ externalApi/sendPasswordResetMail.js
@@ -0,0 +1,6 @@
+const sendPasswordResetMail = (token) => {
+  // 実際のアプリケーションではトークンが入ったパスワード再設定用のメールを送信する
+  console.log('パスワードリセット用のメールを送信', token)
+}
+
+module.exports = sendPasswordResetMail;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { v4: uuid } = require("uuid");
 const cookieParser = require("cookie-parser");
 const { Pool } = require("pg");
 const bcrypt = require("bcrypt");
+const sendPasswordResetMail = require("./externalApi/sendPasswordResetMail");
 
 const port = 3000;
 
@@ -21,6 +22,13 @@ const pool = new Pool({
   password: "password",
   port: 5432,
 });
+
+const isExpiredToken = (tokenCreatedDateString) => {
+  const now = new Date().getTime();
+  const tokenCreatedDate = new Date(tokenCreatedDateString).getTime();
+  const tokenElapsedHour = (now - tokenCreatedDate) / (60 * 60 * 1000);
+  return tokenElapsedHour > 1;
+};
 
 // ログインページを表示する
 app.get("/login", (req, res) => {
@@ -63,16 +71,16 @@ app.post("/login", async (req, res) => {
   const { rows } = await connect.query("SELECT * FROM users WHERE email=$1", [
     email,
   ]);
-  connect.release();
+  await connect.release();
 
   if (bcrypt.compareSync(password, rows[0].password)) {
     return res
       .status(401)
-      .json({ msg: "メールアドレスまたはパスワードが間違っています" });
+      .json({ message: "メールアドレスまたはパスワードが間違っています" });
   }
 
   if (rows.length <= 0) {
-    return res.status(401).json({ msg: "認証に失敗しました" });
+    return res.status(401).json({ message: "認証に失敗しました" });
   }
 
   const token = jwt.sign({ userId: rows[0].id }, "secret");
@@ -95,6 +103,90 @@ app.get("/dashboard", async (req, res) => {
   }
 
   res.render("dashboard");
+});
+
+app.get("/password-reset", async (req, res) => {
+  res.render("passwordResetMailSend");
+});
+
+app.get("/password-reset/:token", async (req, res) => {
+  const token = req.params.token;
+
+  const connect = await pool.connect();
+  const { rows: passwordResettings } = await connect.query(
+    "SELECT * FROM password_resettings WHERE token=$1 ORDER BY created_at DESC LIMIT 1",
+    [token]
+  );
+
+  if (passwordResettings.length < 1) {
+    return res.status(400).json({ message: "存在しないページです" });
+  }
+
+  if (isExpiredToken(passwordResettings[0].created_at)) {
+    return res
+      .status(400)
+      .json({ message: "パスワード更新用リンクの期限が切れています" });
+  }
+
+  await connect.release();
+  res.render("passwordReset", { token });
+});
+
+app.post("/password-reset/send-mail", async (req, res) => {
+  const { email } = req.body;
+  const passwordResetToken = uuid();
+
+  const connect = await pool.connect();
+
+  const { rows: users } = await connect.query(
+    "SELECT * FROM users WHERE email=$1",
+    [email]
+  );
+  console.log("not created!");
+  if (users.length <= 0) return res.render("passwordResetMailSent", { email });
+
+  await connect.query(
+    "INSERT INTO password_resettings (token, email) VALUES ($1,$2)",
+    [passwordResetToken, email]
+  );
+  await connect.release();
+  console.log("created!");
+  // パスワード再設定用メールを送信する
+  sendPasswordResetMail(passwordResetToken);
+
+  res.render("passwordResetMailSent", { email });
+});
+
+app.post("/password-reset/:token", async (req, res) => {
+  const { password } = req.body;
+  const token = req.params.token;
+
+  const connect = await pool.connect();
+
+  const { rows: passwordResettings } = await connect.query(
+    "SELECT * FROM password_resettings WHERE token=$1 ORDER BY created_at DESC LIMIT 1",
+    [token]
+  );
+
+  if (passwordResettings.length < 1) {
+    return res.status(500).json({ message: "internal server error." });
+  }
+
+  if (isExpiredToken(passwordResettings[0].created_at)) {
+    return res
+      .status(400)
+      .json({ message: "パスワード更新用リンクの期限が切れています" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await connect.query("UPDATE users SET password=$1 WHERE email=$2", [
+    hashedPassword,
+    passwordResettings[0].email,
+  ]);
+
+  await connect.release();
+
+  res.render("passwordResetCompleted");
 });
 
 app.listen(port, () => {
